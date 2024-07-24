76cadb3d9316668d27b3593b955eaca41b2845f1
Daniel <danhernandez.renteria@gmail.com>
2024-07-24 13:11:55 -0600

    Tercer commit

--- DataScience/pandas/gta.py
+++ DataScience/pandas/gta.py
@@ -5,13 +5,14 @@ import os
 script_directory = os.path.dirname(os.path.abspath(__file__))
 # Guardo archivo en directorio del script
 db_file = os.path.join(script_directory, "gta.db")
-
+# Conecto a mi nuevo archivo
 connection = sqlite3.connect(db_file)
+# Creo mi cursor
 cursor = connection.cursor()
 
 #Para crear la tabla desde cero /year, name, city/ columnas
-cursor.execute("CREATE TABLE gta (year INTEGER, name TEXT, city TEXT)")
-
+cursor.execute("CREATE TABLE gta (year INTEGER, name TEXT, city TEXT)") # (col. Type)
+# lista con tres columnas de values
 release_list = [
     (1997, "Grand Theft Auto", "state of New Guernsey"),
     (1999, "Grand Theft Auto 2", "Anywhere, USA"),
@@ -21,15 +22,15 @@ release_list = [
     (2008, "Grand Theft Auto IV", "Liberty City"),
     (2013, "Grand Theft Auto V", "Los Santos")
 ]  
-#"?" placeholder para los datos de la lista
+#"?" placeholder para los datos de la lista (un ? por cada columna)
 cursor.executemany("INSERT INTO gta VALUES (?,?,?)", release_list)
 
 # Print data base rows
-for row in cursor.execute("SELECT * FROM gta"):
+for row in cursor.execute("SELECT * FROM gta"): # selecciona todos los rows
     print(row)
 print("**********")
-# Imprime el row donde la columna city == "c" = Liberty City
-cursor.execute("SELECT * FROM gta WHERE city = :c", {"c": "Liberty City"})
+# Imprime el row donde la columna city == al valor de 'c' = Liberty City
+cursor.execute("SELECT * FROM gta WHERE city = :c", {"c": "Liberty City"}) # Se define 'c' aqui mismo
 # Toma todas las coincidencias
 gta_search = cursor.fetchall()
 print(gta_search)
@@ -43,8 +44,9 @@ cities_search = cursor.fetchall()
 print(cities_search)
 print("**********")
 # Accedo a los datos guardados en la variable
-# Reemplazo Liberty city con New York
-# Si el valor en cities_search[0][0], reemplazarlo con cities_search[0][1]
+# Si imprimo me puedo ver que Liberty city esta en la fila 0, col. 0. y New York en fila 0, col. 1
+# Reemplazo Liberty city (fila 0, col. 0) con New York (fila 0, col. 1)
+# Si el valor encontrado en ([row], [col.]) es liberty city, se reemplaza con new york
 for i in gta_search:
     adjusted = [cities_search[0][1] if value == cities_search[0][0] else value for value in i]
     print(adjusted)
